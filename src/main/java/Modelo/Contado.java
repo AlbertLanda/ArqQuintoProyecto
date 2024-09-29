12999ed0d4c44d3426a4efcd81811fc86dc0df93
LENOVO <LENOVO@DESKTOP-2H9U3Q9>
2024-09-28 20:26:45 -0500

    Carga tercer trabajo

---
+++ src/main/java/Modelo/Contado.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Contado extends Venta {
+
+    private int N;
+
+    public Contado(String nombreCliente, String numeroRUC, String nombreProducto, int cantidadComprada, int N) {
+        super(nombreCliente, numeroRUC, nombreProducto, cantidadComprada); 
+        this.N = N;
+    }
+
+    public int getN() {
+        return N;
+    }
+
+    public void setN(int N) {
+        this.N = N;
+    }
+
+    public double calculaDescuento(double subtotal) {
+        double descuento = 0.0;
+        if (subtotal < 1000) {
+            descuento = subtotal * 0.05; 
+        } else if (subtotal >= 1000 && subtotal <= 3000) {
+            descuento = subtotal * 0.08;
+        } else if (subtotal > 3000) {
+            descuento = subtotal * 0.12;
+        }
+        return descuento;
+    }
+    
+    public double calcularNeto() {
+        double subtotal = calcularSubtotal();
+        double descuento = calculaDescuento(subtotal);
+        return subtotal - descuento;
+    }
+
+}
